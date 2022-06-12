4f3688d478174c609fe6258500d8e44128010e8b
Allan R. Greffin <48775941+AllanGreffin@users.noreply.github.com>
2022-06-12 15:38:43 -0300

    before adding font-aersome

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmergencyListingComponent } from './emergency-listing/emergency-listing.component';
+import { MainWindowComponent } from './main-window/main-window.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: MainWindowComponent },
+  { path: "emergencies", component: EmergencyListingComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
